426c528b02eba916c94d3efa1117cedbc1d5a432
Lukáš Píšek <lukin.pisek@email.cz>
2025-12-01 20:45:43 +0100

    d1p2 done

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -49,35 +49,61 @@ public class Day1 : IDay
             switch (line[0])
             {
                 case 'L':
-                    zeroCount += AlterDial(-int.Parse(line[1..]), ref dialPos);
+                    zeroCount += AlterDialPartTwo(-int.Parse(line[1..]), ref dialPos);
                     break;
                 case 'R':
-                    zeroCount += AlterDial(int.Parse(line[1..]), ref dialPos);
+                    zeroCount += AlterDialPartTwo(int.Parse(line[1..]), ref dialPos);
                     break;
             }
         }
         
         return zeroCount.ToString();
     }
-
-    private static int AlterDial(int num, ref int dial)
+    
+    private static void AlterDial(int num, ref int dial)
     {
-        int zeroCount = 0;
-        
         dial += num;
         
-        while (dial < 0)
+        if (dial is < 0 or > 99)
         {
-            dial = 100 + dial;
-            zeroCount++;
+            dial %= 100;
+        }
+    }
+
+    //I gave up trying to do this mathematically, just simulating the dial turns instead.
+    //It is not efficient and is a terrible solution, but I've spent way too long on this already.
+    private static int AlterDialPartTwo(int num, ref int dial)
+    {
+        var zeroCount = 0;
+
+        bool isNegative = num < 0;
+        
+        for (int i = 0; i < int.Abs(num); i++)
+        {
+            if (isNegative)
+            {
+                dial -= 1;
+            }
+            else
+            {
+                dial += 1;
+            }
+            
+            if (dial < 0)
+            {
+                dial = 99;
+            }
+            else if (dial > 99)
+            {
+                dial = 0;
+            }
+            
+            if (dial == 0)
+            {
+                zeroCount++;
+            }
         }
         
-        while (dial > 99)
-        {
-            dial -= 100;
-            zeroCount++;
-        }
-
         return zeroCount;
     }
 }
