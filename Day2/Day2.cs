cff7858cb03c9dbb22e65ce1d55a471c0571aefe
Lukáš Píšek <lukin.pisek@email.cz>
2025-12-02 16:34:13 +0100

    d2p2 done

--- Day2/Day2.cs
+++ Day2/Day2.cs
@@ -1,5 +1,6 @@
 ﻿namespace AOC25.Day2;
 
+//Really inefficient solution but it's acceptable for me
 public class Day2 : IDay
 {
     private string[] GetInput()
@@ -18,7 +19,7 @@ public class Day2 : IDay
             var idRanges = id.Split('-');
             for (long i = long.Parse(idRanges[0]); i <= long.Parse(idRanges[1]); i++)
             {
-                if (CheckForInvalid(i))
+                if (CheckForInvalidPartOne(i))
                 {
                     res += i;
                 }
@@ -28,9 +29,9 @@ public class Day2 : IDay
         return res.ToString();
     }
 
-    private bool CheckForInvalid(long id)
+    private static bool CheckForInvalidPartOne(long id)
     {
-        string idStr = id.ToString();
+        var idStr = id.ToString();
         int idLen = idStr.Length;
 
         bool res = idStr[..(idLen / 2)] == idStr[(idLen / 2)..];
@@ -40,6 +41,60 @@ public class Day2 : IDay
     
     public string SolvePartTwo()
     {
-        return "nothing yet";
+        string[] input = GetInput();
+
+        long res = 0;
+        
+        foreach (string id in input)
+        {
+            var idRanges = id.Split('-');
+            for (long i = long.Parse(idRanges[0]); i <= long.Parse(idRanges[1]); i++)
+            {
+                if (CheckForInvalidPartTwo(i))
+                {
+                    res += i;
+                }
+            }
+        }
+
+        return res.ToString();
+    }
+
+    private static bool CheckForInvalidPartTwo(long id)
+    {
+        string idStr = id.ToString();
+        int idLen = idStr.Length;
+
+        bool isInvalid = false;
+        
+        for (var i = 1; i <= idLen / 2; i++)
+        {
+            if (idLen % i != 0)
+            {
+                continue;
+            }
+            
+            List<string> parts = SplitByIndex(idStr,i);
+
+            if (parts.Distinct().Count() == 1)
+            {
+                isInvalid = true;
+                break;
+            }
+        }
+
+        return isInvalid;
+    }
+
+    private static List<string> SplitByIndex(string text, int index)
+    {
+        List<string> parts = [];
+        
+        for (var i = 0; i < text.Length; i += index)
+        {
+            parts.Add(text[i..(i+index)]);
+        }
+
+        return parts;
     }
 }
