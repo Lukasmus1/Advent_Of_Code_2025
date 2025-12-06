92015675cd7e3607d154a33f8f5f71869db798fa
Lukáš Píšek <lukin.pisek@email.cz>
2025-12-06 10:45:12 +0100

    d4p2 done

--- Day4/Day4.cs
+++ Day4/Day4.cs
@@ -71,6 +71,72 @@ public class Day4 : IDay
 
     public string SolvePartTwo()
     {
-        return "";
+        List<string> input = GetInput();
+        List<(int, int)> directions =
+        [
+            (0, 1),
+            (1, 0),
+            (1, 1),
+            (0, -1),
+            (-1, 0),
+            (-1, -1),
+            (1, -1),
+            (-1, 1)
+        ];
+
+        var res = 0;
+
+        bool loopRoll;
+        do
+        {
+            loopRoll = false;
+            for (var x = 0; x < input[0].Length; x++)
+            {
+                for (var y = 0; y < input.Count; y++)
+                {
+                    if (input[x][y] != '@')
+                    {
+                        continue;
+                    }
+
+                    var rollCount = 0;
+                    foreach ((int, int) dir in directions)
+                    {
+                        if (input[x][y] != '@')
+                        {
+                            break;
+                        }
+
+                        if (x + dir.Item1 < 0 ||
+                            x + dir.Item1 >= input[0].Length ||
+                            y + dir.Item2 < 0 ||
+                            y + dir.Item2 >= input.Count)
+                        {
+                            continue;
+                        }
+
+                        if (input[x + dir.Item1][y + dir.Item2] != '@')
+                        {
+                            continue;
+                        }
+
+                        rollCount++;
+                    }
+
+                    if (rollCount < 4)
+                    {
+                        char[] row = input[x].ToCharArray();
+                        row[y] = '.';
+                        input[x] = new string(row);
+                        res++;
+                        
+                        loopRoll = true;
+                    }
+                }
+            }
+        }
+        while (loopRoll);
+
+        return res.ToString();
     }
 }
