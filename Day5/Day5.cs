3dfca6da7b4faff352c1e7530f5fcbd8abd3529e
Lukáš Píšek <lukin.pisek@email.cz>
2025-12-07 18:02:36 +0100

    d5p2 done

--- Day5/Day5.cs
+++ Day5/Day5.cs
@@ -57,6 +57,67 @@ public class Day5 : IDay
 
     public string SolvePartTwo()
     {
-        return "";
+        (List<string> ranged, List<string> ids) = GetInput();
+
+        Dictionary<long, long> ranges = new();
+
+        int fresh = 0;
+
+        foreach (string s in ranged)
+        {
+            string[] split = s.Split('-');
+
+            long start = long.Parse(split[0]);
+            long end = long.Parse(split[1]);
+
+            if (ranges.TryGetValue(start, out long value))
+            {
+                if (value < end)
+                {
+                    ranges[start] = end;
+                }
+            }
+            else
+            {
+                ranges.Add(start, end);
+            }
+        }
+
+        Dictionary<long, long> merged = MergeIdRanges(ranges);
+
+        return merged.Sum(r => r.Value - r.Key + 1).ToString();
+
+    }
+
+    private static Dictionary<long, long> MergeIdRanges(Dictionary<long, long> inputDict)
+    {
+        List<KeyValuePair<long, long>> sortedRanges = inputDict.OrderBy(x => x.Key).ToList();
+        
+        var mergedDict = new Dictionary<long, long>();
+        
+        long currentStart = sortedRanges[0].Key;
+        long currentEnd = sortedRanges[0].Value;
+        
+        for (int i = 1; i < sortedRanges.Count; i++)
+        {
+            long nextStart = sortedRanges[i].Key;
+            long nextEnd = sortedRanges[i].Value;
+
+            if (nextStart <= currentEnd)
+            {
+                currentEnd = Math.Max(currentEnd, nextEnd);
+            }
+            else
+            {
+                mergedDict.Add(currentStart, currentEnd);
+                
+                currentStart = nextStart;
+                currentEnd = nextEnd;
+            }
+        }
+        
+        mergedDict.Add(currentStart, currentEnd);
+
+        return mergedDict;
     }
 }
