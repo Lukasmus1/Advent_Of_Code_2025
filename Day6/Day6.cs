5957f5479eeeaa2fd836144c988d758beb9b3e4e
Lukáš Píšek <lukin.pisek@email.cz>
2025-12-07 16:52:24 +0100

    d6p2 progress

--- Day6/Day6.cs
+++ Day6/Day6.cs
@@ -1,10 +1,19 @@
-﻿namespace AOC25.Day6;
+﻿using System.Text;
+
+namespace AOC25.Day6;
 
 public class Day6 : IDay
 {
     public string GetDay() => "6";
 
-    private List<List<string>> GetInput()
+    private List<List<string>> GetInputPartOne()
+    {
+        List<string> lines = File.ReadAllLines("Day6/input.txt").ToList();
+
+        return lines.Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList()).ToList();
+    }
+    
+    private List<List<string>> GetInputPartTwo()
     {
         List<string> lines = File.ReadAllLines("Day6/input.txt").ToList();
 
@@ -13,7 +22,7 @@ public class Day6 : IDay
     
     public string SolvePartOne()
     {
-        List<List<string>> input = GetInput();
+        List<List<string>> input = GetInputPartOne();
 
         List<long> res = [];
         
@@ -39,6 +48,45 @@ public class Day6 : IDay
 
     public string SolvePartTwo()
     {
-        throw new NotImplementedException();
+        List<List<string>> input = GetInputPartOne();
+
+        List<long> res = [];
+
+        StringBuilder sb = new();
+        
+        for (var x = 0; x < input[0].Count; x++)
+        {
+            List<string> nums = [];
+            nums.AddRange(input.Select(t => t[x]));
+
+            string largest = nums.MaxBy(s => s.Length)!;
+            string operation = nums[^1];
+            
+            List<long> numbers = [];
+            for (int i = 0; i < largest.Length; i++)
+            {
+                for (int numIndex = 0; numIndex < nums.Count - 1; numIndex++)
+                {
+                    if (nums[numIndex].Length - i - 1 < 0)
+                    {
+                        continue;
+                    }
+
+                    sb.Append(nums[numIndex][^(i + 1)]);
+                }
+                
+                numbers.Add(long.Parse(sb.ToString()));
+                sb.Clear();
+            }
+            
+            res.Add(operation switch 
+            {
+                "+" => numbers.Sum(),
+                "*" => numbers.Aggregate((a, b) => a * b),
+                _ => 0
+            });
+        }
+
+        return res.Sum().ToString();
     }
 }
